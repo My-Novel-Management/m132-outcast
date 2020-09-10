4b199313b349860737808d2cca2c31203cf45244
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-09-11 08:56:16 +0900

    Dev lo (#9)
    * Add document
    
    * Add LO
    
    * Update v0.8.0 [ci skip]

--- src/scenes/Gymnasium.py
+++ src/scenes/Gymnasium.py
@@ -10,6 +10,21 @@ from storybuilder.builder.world import World
 
 
 ## scenes
+def bullying(w: World):
+    ako, misora = w.get("ako"), w.get("misora")
+    mizu = w.get("mizu")
+    nori = w.get("nori")
+    return w.scene("いじめ",
+            w.cmd.change_stage("Gymnasium"),
+            w.cmd.change_time("afternoon"),
+            w.plot_note("やがて精神バランスを壊して$noriは入院という名の不登校になった"),
+            "$noriいじめの兆候みたいな描写を入れてから、BR挟んで、翌週から空席ができた、で",
+            "体育館の描写",
+            "体育の時間で、運動が苦手な$noriに対しての嫌がらせ的ボール回し（バレー）",
+            )
+
+
+
 def old_friend(w: World):
     ako = w.get("ako")
     nori = w.get("nori")
--- src/scenes/Room.py
+++ src/scenes/Room.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Stage: "stage name"
+Stage: "亜子の部屋"
 '''
 import os
 import sys
@@ -10,4 +10,11 @@ from storybuilder.builder.world import World
 
 
 ## scenes
-
+def mystery_message(w: World):
+    return w.scene("謎のメッセージ",
+            w.cmd.change_stage("Room"),
+            w.cmd.change_time("night"),
+            "自室の描写を簡単に",
+            "$noriとのメッセージのやり取り",
+            "最後に$sivaからメッセージが来る",
+            )
